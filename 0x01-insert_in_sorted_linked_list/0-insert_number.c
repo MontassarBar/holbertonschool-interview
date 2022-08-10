0a08284732127d5ba964cc8ac9803f4f09a6cb2e
MontassarBar <montassarbarbouchi7@gmail.com>
2022-08-10 17:38:00 +0200

    Insert in sorted linked list

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -10,7 +10,7 @@
  */
 listint_t *insert_node(listint_t **head, int number)
 {
-listint_t *temp = *head;
+listint_t *temp;
 listint_t *node;
 if (*head == NULL || head == NULL)
 	return (NULL);
@@ -18,14 +18,19 @@ node = malloc(sizeof(listint_t));
 if (node == NULL)
 	return (NULL);
 node->n = number;
+node->next = NULL;
 if ((*head) == NULL || (*head)->n > node->n)
 	{
 	node->next = (*head);
 	(*head) = node;
 	}
+else
+{
+temp = *head;
 while (temp != NULL && temp->next->n < node->n)
 	temp = temp->next;
 node->next = temp->next;
 temp->next = node;
-return (temp);
+}
+return (node);
 }
