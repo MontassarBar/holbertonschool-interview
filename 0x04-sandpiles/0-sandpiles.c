3f0e908696cad3c404a6869251a33c13ad01500f
MontassarBar <montassarbarbouchi7@gmail.com>
2022-08-31 15:25:15 +0200

    sandpiles

--- 0x04-sandpiles/0-sandpiles.c
+++ 0x04-sandpiles/0-sandpiles.c
@@ -65,57 +65,43 @@ while (x < 3)
 	}
 }
 /**
- * sandpiles_calc - sandpiles things
- * @grid1: 3x3 grid
- *
- */
-void sandpiles_calc(int grid1[3][3])
-{
-int i, j, f;
-
-i = 0;
-while (i < 3)
-	{
-	j = 0;
-	while (j < 3)
-		{
-		if (grid1[i][j] > 3)
-			{
-			f = 0;
-			if (grid1[i][j + 1] <= 3)
-				f = 1;
-			grid1[i][j] = grid1[i][j] - 4;
-			if (j != 2)
-				grid1[i][j + 1] = grid1[i][j + 1] + 1;
-			if (j != 0)
-				grid1[i][j - 1]	= grid1[i][j - 1] + 1;
-			if (i != 2)
-				grid1[i + 1][j] = grid1[i + 1][j] + 1;
-			if (i != 0)
-				grid1[i - 1][j]	= grid1[i - 1][j] + 1;
-			if (f == 1)
-				j++;
-			}
-		j++;
-		}
-	i++;
-	}
-}
-/**
  * sandpiles_sum - computes the sum of two sandpiles
  * @grid1: 3x3 grid
  * @grid2: 3x3 grid
  */
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
+int j, i;
+
 grid_sum(grid1, grid2);
-if (check_grid_stability(grid1) == 1)
+	if (check_grid_stability(grid1) == 1)
 	{
 	printf("=\n");
 	print_grid(grid1);
 	while (0 < 1)
 	{
-		sandpiles_calc(grid1);
+	i = 0;
+	while (i < 3)
+		{
+		j = 0;
+		while (j < 3)
+			{
+			if (grid1[i][j] > 3)
+				{
+				grid1[i][j] = grid1[i][j] - 4;
+				if (j != 2)
+					grid1[i][j + 1] = grid1[i][j + 1] + 1;
+				if (j != 0)
+					grid1[i][j - 1]	= grid1[i][j - 1] + 1;
+				if (i != 2)
+					grid1[i + 1][j] = grid1[i + 1][j] + 1;
+				if (i != 0)
+					grid1[i - 1][j]	= grid1[i - 1][j] + 1;
+				}
+				j++;
+			}
+		i++;
+		}
 		if (check_grid_stability(grid1) == 1)
 		{
 		printf("=\n");
