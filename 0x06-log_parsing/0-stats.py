1e8333211b6fcf4add426bb1417ba0afbe7a8b25
MontassarBar <montassarbarbouchi7@gmail.com>
2022-09-15 14:45:54 +0200

    Log parsing

---
+++ 0x06-log_parsing/0-stats.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+from multiprocessing.sharedctypes import Value
+import sys
+
+lines = 0
+file_size = 0
+dict = {"200": 0, "301": 0, "400": 0, "401": 0,
+        "403": 0, "404": 0, "405": 0, "500": 0}
+try:
+    for line in sys.stdin:
+        splited_line = line.split(" ")
+        if len(splited_line) == 9:
+            lines += 1
+            file_size += int(splited_line[8])
+            for key in dict.keys():
+                if (key == splited_line[7]):
+                    dict[key] += 1
+            if lines == 10:
+                lines = 0
+                print("File size:", file_size)
+                for key, value in dict.items():
+                    print(key + ": " + str(value))
+except KeyboardInterrupt:
+    print("File size:", file_size)
+    for key, value in dict.items():
+        print(key + ": " + str(value))
