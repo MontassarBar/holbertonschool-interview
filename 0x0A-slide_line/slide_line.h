a7bc8ad5b0ed775ed952082c9d63e046925a3e40
MontassarBar <montassarbarbouchi7@gmail.com>
2022-10-13 19:15:42 +0200

    header file

--- 0x0A-slide_line/slide_line.h
+++ 0x0A-slide_line/slide_line.h
@@ -1,9 +1,10 @@
-#ifndef SLIDELINE_H
-#define SLIDELINE_H
-#define SLIDE_LEFT 1
-#define SLIDE_RIGHT 2
+#ifndef SLIDE_LINE_H
+#define SLIDE_LINE_H
+
 #include <stddef.h>
 
+#define SLIDE_LEFT 0
+#define SLIDE_RIGHT 1
 int slide_line(int *line, size_t size, int direction);
 
 #endif
