002cb911cb17e1992ac3902e2e48e3ff0b11d5d6
MontassarBar <montassarbarbouchi7@gmail.com>
2023-03-31 00:25:05 +0200

    AVL - From sorted array

--- 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
+++ 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -16,11 +16,9 @@ if (root_node == NULL)
 	return (NULL);
 if (left > right)
 	return (NULL);
-mid = left + (right - left) / 2;
+mid = left + right / 2;
 root_node->parent = parent;
 root_node->n = array[mid];
-root_node->left = NULL;
-root_node->right = NULL;
 root_node->left = convert_array_to_AVL(array, left, mid - 1, root_node);
 root_node->right = convert_array_to_AVL(array, mid + 1, right, root_node);
 return (root_node);
