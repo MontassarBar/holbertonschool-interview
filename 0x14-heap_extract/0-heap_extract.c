e797250e5febf2f8a7e31341c31b6537ec32d9a1
MontassarBar <montassarbarbouchi7@gmail.com>
2023-05-17 19:25:57 +0200

    Heap - Extract

--- 0x14-heap_extract/0-heap_extract.c
+++ 0x14-heap_extract/0-heap_extract.c
@@ -3,78 +3,77 @@
 #include <stdio.h>
 
 /**
-*swap - swap 2 nodes values
-*@node_1: node_1
-*@node_2: node_2
-**/
-void swap(heap_t *node_1, heap_t *node_2)
-{
-	int temp = node_1->n;
-
-	node_1->n = node_2->n;
-	node_2->n = temp;
-}
-
-/**
-*heapify - creating a heap data structure from a binary tree
-*@root: root node
-**/
-void heapify(heap_t *root)
-{
-	if (!root)
-		return;
-	if (root->right && (root->n < root->right->n))
-	{
-	swap(root, root->right);
-	heapify(root->right);
-	}
-	else if (root->left && (root->n < root->left->n))
-	{
-	swap(root, root->left);
-	heapify(root->left);
-	}
-}
-
-/**
-* minHeap - get the min node of the heap
-* @root: root node
-* Return: min node
-**/
-heap_t *minHeap(heap_t *root)
-{
-	heap_t *min_node;
-
-	if (!root)
-		return (0);
-	if (!root->left && !root->right)
-		return (root);
-	if (root->left && (!root->right || root->left->n >= root->right->n))
-		min_node = root->left;
-	else
-		min_node = root->right;
-	swap(root, min_node);
-	return (minHeap(min_node));
-}
-
-/**
-* heap_extract - extracts the root node of a Max Binary Heap
+* get_down - get the root to the last element.
 * @root: root
-* Return: the value stored in the root node
+* Return: last elemnt
+*/
+heap_t *get_down(heap_t *root)
+{
+	heap_t *child;
+	int n;
+
+	if (root->left == NULL && root->right == NULL)
+	{
+		return (root);
+	}
+	if (root->left &&
+		(!root->right || root->left->n >= root->right->n))
+		child = root->left;
+	else
+		child = root->right;
+	n = root->n;
+	root->n = child->n;
+	child->n = n;
+	return (get_down(child));
+}
+
+/**
+* heap_rebalance - rebalance heap by checking if
+* left nodes have more or the same nodes as right nodes
+* @root: root of heap
 **/
+void heap_rebalance(heap_t *root)
+{
+	heap_t *lchild = root->left, *rchild = root->right;
+
+	if (rchild == NULL)
+		return;
+
+	if (lchild->left == NULL && rchild->left != NULL)
+	{
+		lchild->left = rchild->left;
+		rchild->left->parent = lchild;
+		rchild->left = NULL;
+	}
+	if (lchild->right == NULL && rchild->right != NULL)
+	{
+		lchild->right = rchild->right;
+		rchild->right->parent = lchild;
+		rchild->right = NULL;
+	}
+	heap_rebalance(root->left);
+	heap_rebalance(root->right);
+}
+
+/**
+ * heap_extract - extract root and re organize tree
+ * @root: root of tree
+ * Return: return value of root
+ */
 int heap_extract(heap_t **root)
 {
-	heap_t *min_node;
-	int value;
+	heap_t *last;
+	int n;
 
-	if (!(*root) || !root)
+	if (root == NULL || *root == NULL)
 		return (0);
-	min_node = minHeap(*root);
-	value = min_node->n;
-	if (min_node->parent->left == min_node)
-		min_node->parent->left = NULL;
+	last = get_down(*root);
+	n = last->n;
+	if (last->parent->right == last)
+		last->parent->right = NULL;
 	else
-		min_node->parent->right = NULL;
-	free(min_node);
-	heapify(*root);
-	return (value);
+		last->parent->left = NULL;
+	free(last);
+	heap_rebalance(*root);
+	return (n);
 }
