0116d270903e492f62d5fce4dca9cc3b88d422e1
MontassarBar <montassarbarbouchi7@gmail.com>
2023-05-17 19:00:03 +0200

    Heap - Extract

--- 0x14-heap_extract/0-heap_extract.c
+++ 0x14-heap_extract/0-heap_extract.c
@@ -21,19 +21,17 @@ void swap(heap_t *node_1, heap_t *node_2)
 **/
 void heapify(heap_t *root)
 {
-	heap_t *tmp = root;
-
-	if (!tmp)
+	if (!root)
 		return;
-	if (tmp->right && (tmp->n < tmp->right->n))
+	if (root->right && (root->n < root->right->n))
 	{
-	swap(tmp, tmp->right);
-	heapify(tmp->right);
+	swap(root, root->right);
+	heapify(root->right);
 	}
-	else if (tmp->left && (tmp->n < tmp->left->n))
+	else if (root->left && (root->n < root->left->n))
 	{
-	swap(tmp, tmp->left);
-	heapify(tmp->left);
+	swap(root, root->left);
+	heapify(root->left);
 	}
 }
 
@@ -44,18 +42,17 @@ void heapify(heap_t *root)
 **/
 heap_t *minHeap(heap_t *root)
 {
-	heap_t *tmp = root;
 	heap_t *min_node;
 
-	if (!tmp)
+	if (!root)
 		return (0);
-	if (!tmp->left && !tmp->right)
-		return (tmp);
-	if (tmp->left && (!tmp->right || tmp->left->n >= tmp->right->n))
-		min_node = tmp->left;
+	if (!root->left && !root->right)
+		return (root);
+	if (root->left && (!root->right || root->left->n >= root->right->n))
+		min_node = root->left;
 	else
-		min_node = tmp->right;
-	swap(tmp, min_node);
+		min_node = root->right;
+	swap(root, min_node);
 	return (minHeap(min_node));
 }
 
@@ -66,17 +63,18 @@ heap_t *minHeap(heap_t *root)
 **/
 int heap_extract(heap_t **root)
 {
-	heap_t *tmp = *root;
-	heap_t *min_node = minHeap(tmp);
-	int value = min_node->n;
+	heap_t *min_node;
+	int value;
 
-	if (tmp == NULL || tmp == NULL)
+	if (*root == NULL || root == NULL)
 		return (0);
+	min_node = minHeap(*root);
+	value = min_node->n;
 	if (min_node->parent->left == min_node)
 		min_node->parent->left = NULL;
 	else
 		min_node->parent->right = NULL;
 	free(min_node);
-	heapify(tmp);
+	heapify(*root);
 	return (value);
 }
