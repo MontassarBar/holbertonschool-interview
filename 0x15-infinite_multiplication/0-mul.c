5f93183ecb86791788133450ae56fe77d8d47147
MontassarBar <montassarbarbouchi7@gmail.com>
2023-05-25 00:55:57 +0200

    Infinite Multiplication

--- 0x15-infinite_multiplication/0-mul.c
+++ 0x15-infinite_multiplication/0-mul.c
@@ -1,6 +1,5 @@
 #include "holberton.h"
 #include <stdlib.h>
-#include <stdio.h>
 
 /**
  * _malloc - allocates memory for an array and initializes it
@@ -134,4 +133,54 @@ int main(int argc, char *argv[])
 	free(num_2);
 	free(result);
 	return (0);
+}
+
+/**
+ * _strrev - reverse a string
+ *
+ * @str: str
+ * Return: str
+ */
+char *_strrev(char *str)
+{
+char tmp;
+int x;
+
+for (x = 0; x < _strlen(str) / 2; x++)
+{
+	tmp = str[x];
+	str[x] = str[_strlen(str) - 1 - x];
+	str[_strlen(str) - 1 - x] = tmp;
+}
+return (str);
+}
+
+/**
+ * _strlen - count the lenght of a string
+ *
+ * @str: str
+ * Return: len
+ */
+int _strlen(char *str)
+{
+	int x;
+
+	x = 0;
+	while (str[x])
+		x++;
+	return (x);
+}
+
+/**
+ * error - error
+ */
+void error(void)
+{
+		_putchar('E');
+		_putchar('r');
+		_putchar('r');
+		_putchar('o');
+		_putchar('r');
+		_putchar('\n');
+		exit(98);
 }
