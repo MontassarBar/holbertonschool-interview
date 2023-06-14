00635f7d215b42fbc17be766ad8f09eea97de6ca
MontassarBar <montassarbarbouchi7@gmail.com>
2023-06-14 13:52:23 +0200

    Find the loop

--- 0x17-find_the_loop/0-find_loop.c
+++ 0x17-find_the_loop/0-find_loop.c
@@ -29,12 +29,10 @@ listint_t *find_listint_loop(listint_t *head)
 		return (NULL);
 	}
 	ptr1 = head;
-	while (ptr1->next && ptr2->next)
+	while (ptr1 != ptr2)
 	{
 		ptr1 = ptr1->next;
 		ptr2 = ptr2->next;
-		if (ptr1 == ptr2)
-			break;
 	}
 	return (ptr1);
 }
