fac0e75fcbeee5b205f6b8b9450dd044ed378d13
MontassarBar <montassarbarbouchi7@gmail.com>
2023-07-20 18:38:15 +0200

    Radix sort

--- 0x1B-radix_sort/0-radix_sort.c
+++ 0x1B-radix_sort/0-radix_sort.c
@@ -1,80 +1,63 @@
 #include "sort.h"
-#include <stdio.h>
-#include <stdlib.h>
-
 
 /**
-* find_max - Find the maximum element in an integer array
-* @arr: Array of integers
-* @n: Number of elements in the array
-* Return: Maximum element in the array
+* get_max - gets max value from array
+* @array: array to pull max from
+* @size: size of array
+* Return: max value
 */
-int find_max(int *arr, int n)
-{
-	int max_element = arr[0];
-	int i;
 
-	for (i = 1; i < n; i++)
+int get_max(int *array, size_t size)
+{
+	int max = array[0], i;
+
+	for (i = 1; i < (int)size; i++)
 	{
-		if (arr[i] > max_element)
-		{
-			max_element = arr[i];
-		}
+		if (array[i] > max)
+			max = array[i];
 	}
-	return (max_element);
+	return (max);
 }
 
 /**
-* countingSort - Sort an integer array using the counting sort algorithm
-* @array: Array of integers to be sorted
-* @n: Number of elements in the array
-* @pos: Position of the digit to be sorted
-* Return: None
-*/
-void countingSort(int *array, int n, int pos)
-{
-	int *result = malloc(sizeof(int) * (n + 1));
-	int count[10] = {0};
-	int i;
-
-
-	for (i = 0; i < n; i++)
-		count[(array[i] / pos) % 10]++;
-
-
-	for (i = 1; i < 10; i++)
-		count[i] += count[i - 1];
-
-
-	for (i = n - 1; i >= 0; i--)
-	{
-		result[count[(array[i] / pos) % 10] - 1] = array[i];
-		count[(array[i] / pos) % 10]--;
-	}
-
-	for (i = 0; i < n; i++)
-		array[i] = result[i];
-
-	free(result);
-}
-
-
-/**
-* radix_sort - Sort an integer array using the radix sort algorithm
-* @array: Array of integers to be sorted
-* @size: Number of elements in the array
-* Return: None
+* radix_sort - sorts via radix algo
+* @array: array to sor
+* @size: size of array
 */
 
 void radix_sort(int *array, size_t size)
 {
+	int bucket[1000][1000], count[10], i = 0, j = 0, k = 0, r = 0;
+	int digits = 0, div = 1, max = 0, p = 0;
 
-	int max_element = find_max(array, size);
-	int pos;
-
-	for (pos = 1; max_element / pos > 0; pos *= 10)
+	if (size < 2)
+		return;
+	max = get_max(array, size);
+	while (max > 0)
 	{
-		countingSort(array, size, pos);
+		digits++;
+		max /= 10;
+	}
+	for (p = 0; p < digits; p++)
+	{
+		for (i = 0; i < 10; i++)
+			count[i] = 0;
+		for (i = 0; i < (int)size; i++)
+		{
+			r = (array[i] / div) % 10;
+			bucket[r][count[r]] = array[i];
+			count[r] += 1;
+		}
+		i = 0;
+		for (k = 0; k < 10; k++)
+		{
+			for (j = 0; j < count[k]; j++)
+			{
+				array[i] = bucket[k][j];
+				i++;
+			}
+		}
+		div *= 10;
 		print_array(array, size);
 	}
 }
