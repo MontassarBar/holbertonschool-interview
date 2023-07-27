6d5db746419ae4656202d4d438200bc2a73990c0
MontassarBar <montassarbarbouchi7@gmail.com>
2023-07-27 18:34:25 +0200

    Island Perimeter

--- 0x1C-island_perimeter/0-island_perimeter.py
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -4,6 +4,8 @@
 
 def island_perimeter(grid):
     '''function that returns the perimeter of the island described in grid'''
+    if not grid:
+        return 0
     p = 0
     for x in range(len(grid)):
         for y in range(len(grid[x])):
