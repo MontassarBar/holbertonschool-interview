6e2a99bc44ea63722597640dc0dcaaa3d71c9fdd
MontassarBar <montassarbarbouchi7@gmail.com>
2023-07-27 18:49:06 +0200

    Island Perimeter

--- 0x1C-island_perimeter/0-island_perimeter.py
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -10,14 +10,7 @@ def island_perimeter(grid):
     for x in range(len(grid)):
         for y in range(len(grid[x])):
             if grid[x][y] == 1:
-                if grid[x][y + 1] == 0 and x:
-                    p += 1
-                if grid[x][y - 1] == 0:
-                    p += 1
-                if grid[x + 1][y] == 0:
-                    p += 1
-                if grid[x - 1][y] == 0:
-                    p += 1
+                p += 4
                 if x > 0 and grid[x - 1][y] == 1:
                     p -= 2
                 if y > 0 and grid[x][y - 1] == 1:
